70e70ae921ffe81ffc677de7e14b86859127ebef
LucasBarduino <lucas12barduino@gmail.com>
2026-05-22 15:32:59 -0300

    filtro dos numeros

--- Treinamento/src/app/components/home/home.ts
+++ Treinamento/src/app/components/home/home.ts
@@ -27,18 +27,26 @@ export class Home implements OnInit {
       this.cdr.detectChanges();
     });
   }
+//
 
-  adicionar() {
-    if (this.nome && this.numero) {
-      const novoContato: Contato = { nome: this.nome, numero: this.numero };
-      this.api.adicionarContato(novoContato).subscribe(contatoSalvo => {
-        this.contatos = [...this.contatos, contatoSalvo];
-        this.nome = '';
-        this.numero = '';
-        this.cdr.detectChanges();
-      });
+adicionar() {
+  if (this.nome && this.numero) {
+    const limpo = this.numero.replace(/[\s\-()]/g, '');
+    
+    if (!/^\d{10,13}$/.test(limpo)) {
+      alert('Número inválido. Informe tamebém o país e o DDD Ex: 55 31 9 99999999');
+      return;
     }
+
+    const novoContato: Contato = { nome: this.nome, numero: limpo };
+    this.api.adicionarContato(novoContato).subscribe(contatoSalvo => {
+      this.contatos = [...this.contatos, contatoSalvo];
+      this.nome = '';
+      this.numero = '';
+      this.cdr.detectChanges();
+    });
   }
+}
 
   deletar(id: number) {
     this.api.deletarContato(id).subscribe(() => {
