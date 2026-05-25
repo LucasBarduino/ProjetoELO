e6bd02af23c50176f6750f1c29682d32bfeb5cb9
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2026-05-25 19:10:39 -0300

    update

--- Treinamento/src/app/app.ts
+++ Treinamento/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './components/home/home';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Home],
+  imports: [RouterOutlet, Home,CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
