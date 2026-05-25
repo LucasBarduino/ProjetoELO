85930bc49eb62b996481365ffbf2c09eb7eb839c
LucasBarduino <lucas12barduino@gmail.com>
2026-05-24 21:39:29 -0300

    mudanças vizuais

--- Treinamento/src/app/components/home/home.ts
+++ Treinamento/src/app/components/home/home.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ApiService, Contato } from '../../services/api';
-import id from '@angular/common/locales/extra/id';
+
+type ContatoUI = Contato & { editando: boolean; _backup?: Contato };
 
 @Component({
   selector: 'app-home',
@@ -14,12 +15,9 @@ import id from '@angular/common/locales/extra/id';
 export class Home implements OnInit {
   nome = '';
   numero = '';
-  contatos: Contato[] = [];
+  contatos: ContatoUI[] = [];
 
-  constructor(
-    private api: ApiService,
-    private cdr: ChangeDetectorRef,
-  ) {}
+  constructor(private api: ApiService) {}
 
   ngOnInit() {
     this.carregarContatos();
@@ -27,44 +25,51 @@ export class Home implements OnInit {
 
   carregarContatos() {
     this.api.getContatos().subscribe((resultado) => {
-      this.contatos = resultado;
-      this.cdr.detectChanges();
+      this.contatos = resultado.map((c) => ({ ...c, editando: false }));
     });
   }
-  //
 
   adicionar() {
     if (this.nome && this.numero) {
       const limpo = this.numero.replace(/[\s\-()]/g, '');
-
       if (!/^\d{10,13}$/.test(limpo)) {
-        alert('Número inválido. Informe tamebém o país e o DDD Ex: 55 31 9 99999999');
+        alert('Número inválido. Informe também o país e o DDD. Ex: 55 31 9 99999999');
         return;
       }
-
       const novoContato: Contato = { nome: this.nome, numero: limpo };
       this.api.adicionarContato(novoContato).subscribe((contatoSalvo) => {
-        this.contatos = [...this.contatos, contatoSalvo];
+        this.contatos = [...this.contatos, { ...contatoSalvo, editando: false }];
         this.nome = '';
         this.numero = '';
-        this.cdr.detectChanges();
       });
     }
   }
 
-  editar(contato: Contato) {
+  ativarEdicao(contato: ContatoUI) {
+    this.contatos = this.contatos.map((c) =>
+      c.id === contato.id
+        ? { ...c, editando: true, _backup: { id: c.id, nome: c.nome, numero: c.numero } }
+        : c,
+    );
+  }
+
+  salvar(contato: ContatoUI) {
     this.api.editarContato(contato).subscribe((contatoAtualizado) => {
       this.contatos = this.contatos.map((c) =>
-        c.id === contatoAtualizado.id ? contatoAtualizado : c,
+        c.id === contatoAtualizado.id ? { ...contatoAtualizado, editando: false } : c,
       );
-      this.cdr.detectChanges();
     });
+  }
+
+  cancelar(contato: ContatoUI) {
+    this.contatos = this.contatos.map((c) =>
+      c.id === contato.id && c._backup ? { ...c._backup, editando: false } : c,
+    );
   }
 
   deletar(id: number) {
     this.api.deletarContato(id).subscribe(() => {
       this.contatos = this.contatos.filter((c) => c.id !== id);
-      this.cdr.detectChanges();
     });
   }
 }
