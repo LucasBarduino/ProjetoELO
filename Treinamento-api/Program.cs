b00e6b6da32c9b1eccc4d54f20d4bdec3a7c2506
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2026-05-25 17:35:28 -0300

    update

--- Treinamento-api/Program.cs
+++ Treinamento-api/Program.cs
@@ -4,25 +4,37 @@ using treinamento_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Troque isso:
-// opt.UseSqlite("Data Source=contatos.db")
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-// Por isso:
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IContatoService, ContatoService>();
 builder.Services.AddControllers();
 
-builder.Services.AddCors(opt =>
-    opt.AddDefaultPolicy(p =>
-        p.WithOrigins("http://localhost:4200")
-         .AllowAnyMethod()
-         .AllowAnyHeader()));
+// 1. Definimos APENAS a política "PermitirTudo" para não haver conflitos
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTudo", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-app.UseCors();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// 2. Obrigatório: Chamamos o CORS usando exatamente o NOME da política que criamos
+app.UseCors("PermitirTudo");
+
 app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
